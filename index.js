ca7e92c4005da7bcc1e1cca297025019ec824d47
G-Gautam <gautamgupta9298@gmail.com>
2020-11-26 18:21:21 -0500

    Devtool extension pivot

--- index.js
+++ index.js
@@ -2,4 +2,22 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({ color: '#3aa757' }, function() {
         console.log("The color is green.");
     });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: { urlMatches: '.*' },
+            })],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
 });
+
+
+
+// var overlay = document.createElement('div');
+// overlay.style.width = '100vw';
+// overlay.style.height = '100vh';
+// overlay.style.backgroundColor = 'black';
+// overlay.style.opacity = '0.5';
+// overlay.style.position = 'fixed';
+// overlay.style.z-index = '9999';
